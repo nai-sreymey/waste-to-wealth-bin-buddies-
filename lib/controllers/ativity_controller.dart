ef1c9e0ba5adcda56bfa2cb03187546fa8706650
sornmonika <monika.sorn@institute.pse.ngo>
2025-03-25 08:39:17 +0700

    activity

---
+++ lib/controllers/ativity_controller.dart
@@ -0,0 +1,12 @@
+
+
+// import 'package:waste_friendly/models/ativity_model.dart';
+// import 'package:waste_friendly/services/api_service.dart';
+
+// class ActivityController {
+//   final ApiService _apiService = ApiService();
+
+//   Future<List<Activity>> fetchActivities() {
+//     return _apiService.fetchActivity();
+//   }
+// }
